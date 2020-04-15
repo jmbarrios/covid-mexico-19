eabdb1da8af208277804b5e110246a15b4021099
Everardo <erobredo@conabio.gob.mx>
2020-04-14 21:08:10 -0500

    Serializers

--- covid_api/serializers.py
+++ covid_api/serializers.py
@@ -2,6 +2,12 @@ from covid_data import models
 from rest_framework import serializers
 
 
+class SexoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Sexo
+        fields = ['clave', 'nombre']
+
+
 class OrigenSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Origen
@@ -20,12 +26,73 @@ class EntidadSerializer(serializers.ModelSerializer):
         fields = ['clave', 'nombre']
 
 
+class NacionalidadSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Nacionalidad
+        fields = ['clave', 'valor']
+
+
+class TipoPacienteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.TipoPaciente
+        fields = ['clave', 'valor']
+
+
+class ResultadoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Resultado
+        fields = ['clave', 'valor']
+
+
+class PaisSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Pais
+        fields = ['nombre', 'codigo', 'region']
+
+
+class MunicipioSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Municipio
+        fields = ['clave', 'clave_municipio',
+                  'abreviatura', 'entidad', 'nombre']
+
+
+class SiNoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.SiNo
+        fields = ['clave', 'valor']
+
+
 class CasoSerializer(serializers.ModelSerializer):
     origen = OrigenSerializer(read_only=True)
     sector = SectorSerializer(read_only=True)
     entidad_um = EntidadSerializer(read_only=True)
-    # sector = serializers.SlugRelatedField(read_only=True, slug_field='valor')
-    # origen = serializers.SlugRelatedField(read_only=True, slug_field='valor')
+    sexo = SexoSerializer(read_only=True)
+    entidad_nacimiento = EntidadSerializer(read_only=True)
+    entidad_residencia = EntidadSerializer(read_only=True)
+    municipio_residencia = MunicipioSerializer(read_only=True)
+    tipo_paciente = SiNoSerializer(read_only=True)
+    intubado = SiNoSerializer(read_only=True)
+    neumonia = SiNoSerializer(read_only=True)
+    nacionalidad = NacionalidadSerializer(read_only=True)
+    embarazo = SiNoSerializer(read_only=True)
+    habla_lengua_indigena = SiNoSerializer(read_only=True)
+    diabetes = SiNoSerializer(read_only=True)
+    epoc = SiNoSerializer(read_only=True)
+    asma = SiNoSerializer(read_only=True)
+    inmusupr = SiNoSerializer(read_only=True)
+    hipertension = SiNoSerializer(read_only=True)
+    otras_com = SiNoSerializer(read_only=True)
+    cardiovascular = SiNoSerializer(read_only=True)
+    obesidad = SiNoSerializer(read_only=True)
+    renal_cronica = SiNoSerializer(read_only=True)
+    tabaquismo = SiNoSerializer(read_only=True)
+    otro_caso = SiNoSerializer(read_only=True)
+    resultado = ResultadoSerializer(read_only=True)
+    migrante = SiNoSerializer(read_only=True)
+    pais_nacionalidad = PaisSerializer(read_only=True)
+    pais_origen = PaisSerializer(read_only=True)
+    uci = SiNoSerializer(read_only=True)
 
     class Meta:
         model = models.Caso
