e3428d9666693585e2dedddf81a4213f853dda52
Everardo <erobredo@conabio.gob.mx>
2020-04-14 22:14:08 -0500

    Geojson serializer

--- django_covid/settings.py
+++ django_covid/settings.py
@@ -35,9 +35,10 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'rest_framework',
     'django_filters',
-    # 'covid_api.apps.CovidApiConfig',
+    'covid_api.apps.CovidApiConfig',
     'covid_data.apps.CovidDataConfig',
     'covid_update.apps.CovidUpdateConfig',
+    'django-geojson_serializer',
     'django.contrib.gis',
     'django.contrib.admin',
     'django.contrib.auth',
