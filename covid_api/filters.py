532195ea934c764c14a30d3f199c7a7bff1bc661
Everardo <erobredo@conabio.gob.mx>
2020-04-14 21:35:46 -0500

    Agrega 'endpoints' para otros modelos

--- covid_api/filters.py
+++ covid_api/filters.py
@@ -1,22 +1,83 @@
 from django_filters import FilterSet
-from covid_data.models import Caso
+from covid_data import models
+
+
+class SexoFilter(FilterSet):
+    class Meta:
+        model = models.Sexo
+        fields = ['clave', 'descripcion']
+
+
+class OrigenFilter(FilterSet):
+    class Meta:
+        model = models.Origen
+        fields = ['clave', 'descripcion']
+
+
+class SectorFilter(FilterSet):
+    class Meta:
+        model = models.Sector
+        fields = ['clave', 'descripcion']
+
+
+class EntidadFilter(FilterSet):
+    class Meta:
+        model = models.Entidad
+        fields = ['clave', 'descripcion']
+
+
+class NacionalidadFilter(FilterSet):
+    class Meta:
+        model = models.Nacionalidad
+        fields = ['clave', 'descripcion']
+
+
+class TipoPacienteFilter(FilterSet):
+    class Meta:
+        model = models.TipoPaciente
+        fields = ['clave', 'descripcion']
+
+
+class ResultadoFilter(FilterSet):
+    class Meta:
+        model = models.Resultado
+        fields = ['clave', 'descripcion']
+
+
+class PaisFilter(FilterSet):
+    class Meta:
+        model = models.Pais
+        fields = ['clave', 'codigo', 'region', 'descripcion']
+
+
+class MunicipioFilter(FilterSet):
+    class Meta:
+        model = models.Municipio
+        fields = ['clave', 'clave_municipio',
+                  'abreviatura', 'entidad', 'descripcion']
+
+
+class SiNoFilter(FilterSet):
+    class Meta:
+        model = models.SiNo
+        fields = ['clave', 'descripcion']
 
 
 class CasoFilter(FilterSet):
     class Meta:
-        model = Caso
+        model = models.Caso
         fields = {
             'fecha_actualizacion': ['gt', 'lt', 'gte', 'lte'],
-            'origen__valor': ['exact', 'contains'],
-            'sector__valor': ['exact', 'contains'],
-            'entidad_um__nombre': ['exact', 'contains'],
+            'origen__descripcion': ['exact', 'contains'],
+            'sector__descripcion': ['exact', 'contains'],
+            'entidad_um__descripcion': ['exact', 'contains'],
             'sexo': ['exact'],
-            'entidad_nacimiento__nombre': ['exact', 'contains'],
-            'entidad_residencia__nombre': ['exact', 'contains'],
-            'municipio_residencia__nombre': ['exact', 'contains'],
+            'entidad_nacimiento__descripcion': ['exact', 'contains'],
+            'entidad_residencia__descripcion': ['exact', 'contains'],
+            'municipio_residencia__descripcion': ['exact', 'contains'],
             'municipio_residencia__clave_municipio': ['exact'],
             'municipio_residencia__abreviatura': ['exact', 'contains'],
-            'tipo_paciente__valor': ['exact', 'contains'],
+            'tipo_paciente__descripcion': ['exact', 'contains'],
             'fecha_ingreso': ['gt', 'lt', 'gte', 'lte'],
             'fecha_sintomas': ['gt', 'lt', 'gte', 'lte'],
             'fecha_defuncion': ['gt', 'lt', 'gte', 'lte'],
@@ -37,12 +98,12 @@ class CasoFilter(FilterSet):
             'renal_cronica': ['exact'],
             'tabaquismo': ['exact'],
             'otro_caso': ['exact'],
-            'resultado__valor': ['exact', 'contains'],
+            'resultado__descripcion': ['exact', 'contains'],
             'migrante': ['exact'],
-            'pais_nacionalidad__nombre': ['exact', 'contains'],
+            'pais_nacionalidad__descripcion': ['exact', 'contains'],
             'pais_nacionalidad__codigo': ['exact', 'contains'],
             'pais_nacionalidad__region': ['exact', 'contains'],
-            'pais_origen__nombre': ['exact', 'contains'],
+            'pais_origen__descripcion': ['exact', 'contains'],
             'pais_origen__codigo': ['exact', 'contains'],
             'pais_origen__region': ['exact', 'contains'],
             'uci': ['exact']
