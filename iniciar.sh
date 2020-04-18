f8f15f7df00bd8fd29667885a95b82c08e96bede
mbsantiago <santiago.mbal@gmail.com>
2020-04-17 23:51:41 -0500

    añadir guinicorn

--- iniciar.sh
+++ iniciar.sh
@@ -2,4 +2,4 @@ python manage.py migrate
 python manage.py actualizar_catalogos --descargar
 python manage.py actualizar_casos --descargar
 cron
-python manage.py runserver 0.0.0.0:8000
+gunicorn django_covid.wsgi:application --bind 0.0.0.0:8000 -w 4
