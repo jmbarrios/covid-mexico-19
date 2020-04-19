a236ebf94f13367c1c6f907cced006ee0d20eb5e
mbsantiago <santiago.mbal@gmail.com>
2020-04-19 16:07:01 -0500

    añadir configuracion para servir archivos estaticos

--- docker/iniciar.prod.sh
+++ docker/iniciar.prod.sh
@@ -4,5 +4,7 @@ python docker/esperar.py
 python manage.py migrate
 python manage.py actualizar_catalogos --descargar
 python manage.py actualizar_casos --descargar
+python manage.py collectstatic --no-input --clear
 cron
-gunicorn django_covid.wsgi:application -w 4 --bind 0.0.0.0:8000
+
+exec "$@"
--- docker/iniciar.sh
+++ docker/iniciar.sh
@@ -4,5 +4,8 @@ python docker/esperar.py
 python manage.py migrate
 python manage.py actualizar_catalogos --descargar
 python manage.py actualizar_casos --descargar
+python manage.py collectstatic --no-input --clear
 cron
 python manage.py runserver 0.0.0.0:8000
+
+exec "$@"
