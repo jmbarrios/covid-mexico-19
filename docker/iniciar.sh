6cab7bec26f2f47dfff71aa176de0b4b47cf5e22
mbsantiago <santiago.mbal@gmail.com>
2020-04-19 16:17:16 -0500

    init scripts

--- docker/iniciar.sh
+++ docker/iniciar.sh
@@ -4,8 +4,6 @@ python docker/esperar.py
 python manage.py migrate
 python manage.py actualizar_catalogos --descargar
 python manage.py actualizar_casos --descargar
-python manage.py collectstatic --no-input --clear
 cron
-python manage.py runserver 0.0.0.0:8000
 
 exec "$@"
